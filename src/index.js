04a83e1a627b07960611771c1b42182f6dfc7906
Markiian <marksanockiy@gmail.com>
2019-04-24 18:20:02 -0400

    State and Lifecycle done

--- src/index.js
+++ src/index.js
@@ -5,44 +5,53 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 function FormattedDate(props) {
-	return <h2>It is {props.date.toLocaleTimerString()}.</h2>;
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
 }
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {date: new Date()};
-	}
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
 
-	componentDidMount() {
-		this.timerID = setInterval(
-			() => this.tick(),
-			1000
-			);
-	}
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-	componentWillMount() {
-		clearInterval(this.timerID);
-	}
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
 
-	tick() {
-		this.setState({
-			date: new Date()
-		});
-	}
-
-	render() {
-	return (
-		<div>
-			<h1>Hello, world!</h1>
-			<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-		</div>
-		);
-			}
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <FormattedDate date={this.state.date} />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<Clock />, document.getElementById('root'));
+function App2() {
+  return (
+    <div>
+      <Clock />
+      <Clock />
+      <Clock />
+    </div>
+  );
+}
+
+ReactDOM.render(<App2 />, document.getElementById('root'));
 
 
 
