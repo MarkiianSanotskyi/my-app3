46521f77a972d53caba3d8c0dc85b9f6125858bb
Markiian <marksanockiy@gmail.com>
2019-04-24 14:58:17 -0400

    Sample of Lifecycle

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'; 
-import FormatName from './FormatName'
 import * as serviceWorker from './serviceWorker';
 
+class Clock extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {date: new Date()};
+	}
 
-ReactDOM.render(<FormatName/>, document.getElementById('root'));
+	render() {
+	return (
+		<div>
+			<h1>Hello, world!</h1>
+			<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+		</div>
+		);
+			}
+}
+
+function tick() {
+ReactDOM.render(<Clock />, document.getElementById('root'));
+
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
