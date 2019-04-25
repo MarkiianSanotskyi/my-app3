59c576a4c188f6cb88cffc1ce32abf24971e8d16
Markiian <marksanockiy@gmail.com>
2019-04-24 22:38:04 -0400

    Unreaded messages

--- src/index.js
+++ src/index.js
@@ -3,75 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
-  }
 
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
 
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
-  }
 
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    let button;
+function Mailbox(props) {
+	const unreadMessages = props.unreadMessages;
 
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
-    }
+	return (
+		<div>
+			<h1>Hello!</h1>
+			{unreadMessages.length > 0 && <h2>You have {unreadMessages.length} unread messages.
+			 </h2>
 
-    return (
-      <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        {button}
-      </div>
-    );
-  }
+			}
+		</div>
+		);
 }
 
-function UserGreeting(props) {
-  return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Please sign up.</h1>;
-}
-
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />;
-}
-
-function LoginButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
-  );
-}
-
-function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-  );
-}
+const messages =['React', 'Re: React', 'Re:Re: React'];
 
 
-ReactDOM.render(<LoginControl />, document.getElementById('root'));
+ReactDOM.render(<Mailbox unreadMessages={messages} />, document.getElementById('root'));
 
 
 
