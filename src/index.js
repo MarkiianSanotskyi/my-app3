dbb414a54e7040d06bc203649d670e5805d66663
Markiian <marksanockiy@gmail.com>
2019-04-24 23:03:58 -0400

    HideAndShowBanner

--- src/index.js
+++ src/index.js
@@ -5,25 +5,46 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-
-function Mailbox(props) {
-	const unreadMessages = props.unreadMessages;
+function WarningBanner(props) {
+	if (!props.warn) {
+		return null;
+	}
 
 	return (
-		<div>
-			<h1>Hello!</h1>
-			{unreadMessages.length > 0 && <h2>You have {unreadMessages.length} unread messages.
-			 </h2>
-
-			}
+		<div className="warning">
+		Warning!
 		</div>
 		);
 }
 
-const messages =['React', 'Re: React', 'Re:Re: React'];
+class Page extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {showWarning: true};
+		this.handleToggleClick = this.handleToggleClick.bind(this);
+	}
+
+	handleToggleClick() {
+		this.setState(state => ({
+			showWarning: !state.showWarning
+		}));
+	}
+
+	render() {
+		return(
+			<div>
+			<WarningBanner warn={this.state.showWarning} />
+			<button onClick={this.handleToggleClick}>
+				{this.state.showWarning ? 'Hide' : 'Show'}
+			</button>
+			</div>
+		);
+	}
+}
 
 
-ReactDOM.render(<Mailbox unreadMessages={messages} />, document.getElementById('root'));
+
+ReactDOM.render(<Page />, document.getElementById('root'));
 
 
 
