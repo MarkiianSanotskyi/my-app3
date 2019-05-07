12d701d04b393b1d900f49a73d4c3b2e4d33b81c
Markiian Sanotskye <marksanockiy@gmail.com>
2019-05-07 15:31:58 -0400

    test

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,31 +3,35 @@ import { Container, StyledUl, StyledLi } from "./style";
 import Card from "./Card/Card";
 
 export const Home = () => (
-  <div>
-    <Container>
-      <StyledUl>
-        <StyledLi>
-          <Card />
-        </StyledLi>
-        <StyledLi>
-          <Card />
-        </StyledLi>
-        <StyledLi>
-          <Card />
-        </StyledLi>
-        <StyledLi>
-          <Card />
-        </StyledLi>
-        <StyledLi>
-          <Card />
-        </StyledLi>
-        <StyledLi>
-          <Card />
-        </StyledLi>
-        <StyledLi>
-          <Card />
-        </StyledLi>
-      </StyledUl>
-    </Container>
-  </div>
+  <Container>
+    <StyledUl>
+      <StyledLi>
+        <Card />
+      </StyledLi>
+      <StyledLi>
+        <Card />
+      </StyledLi>
+      <StyledLi>
+        <Card />
+      </StyledLi>
+      <StyledLi>
+        <Card />
+      </StyledLi>
+      <StyledLi>
+        <Card />
+      </StyledLi>
+      <StyledLi>
+        <Card />
+      </StyledLi>
+      <StyledLi>
+        <Card />
+      </StyledLi>
+
+      <StyledLi>
+        <Card />
+      </StyledLi>
+    </StyledUl>
+  </Container>
 );
+
+var stop = 100;
