503943727174c8b8b42efd8f0abaa23f1ce35c86
Markiian Sanotskye <marksanockiy@gmail.com>
2019-06-18 11:20:51 -0400

    ListItemAndNewLoops

--- src/components/pages/Home/Card/ItemInfoBox/ItemBottomInfo/ItemRating/ItemRating.js
+++ src/components/pages/Home/Card/ItemInfoBox/ItemBottomInfo/ItemRating/ItemRating.js
@@ -2,24 +2,16 @@ import React from "react";
 import { ItemUlRating, ItemLiRating } from "./style";
 import ItemRatingImg from "../../../../../../../assets/star.png";
 
-const ItemRating = () => (
-  <ItemUlRating>
+const ItemRating = () => <ItemUlRating>{raitingList}</ItemUlRating>;
+
+let raitingList = [];
+
+for (let i = 0; i < 5; i++) {
+  raitingList.push(
     <ItemLiRating>
       <img src={ItemRatingImg} alt="ItemRatingImg" />
     </ItemLiRating>
-    <ItemLiRating>
-      <img src={ItemRatingImg} alt="ItemRatingImg" />
-    </ItemLiRating>
-    <ItemLiRating>
-      <img src={ItemRatingImg} alt="ItemRatingImg" />
-    </ItemLiRating>
-    <ItemLiRating>
-      <img src={ItemRatingImg} alt="ItemRatingImg" />
-    </ItemLiRating>
-    <ItemLiRating>
-      <img src={ItemRatingImg} alt="ItemRatingImg" />
-    </ItemLiRating>
-  </ItemUlRating>
-);
+  );
+}
 
 export default ItemRating;
--- src/components/shared/Layout/FooterPayment/FooterPayment.js
+++ src/components/shared/Layout/FooterPayment/FooterPayment.js
@@ -5,22 +5,18 @@ import Logo from "../Logo/Logo";
 const FooterPayment = () => (
   <UlListBar>
     <LiListBar>Secure Payment:</LiListBar>
-    <LiListBar>
-      <Logo />
-    </LiListBar>
-    <LiListBar>
-      <Logo />
-    </LiListBar>
-    <LiListBar>
-      <Logo />
-    </LiListBar>
-    <LiListBar>
-      <Logo />
-    </LiListBar>
-    <LiListBar>
-      <Logo />
-    </LiListBar>
+    {linksFooterPayments}
   </UlListBar>
 );
+
+let linksFooterPayments = [];
+
+for (let i = 0; i < 5; i++) {
+  linksFooterPayments.push(
+    <LiListBar>
+      <Logo />
+    </LiListBar>
+  );
+}
 
 export default FooterPayment;
