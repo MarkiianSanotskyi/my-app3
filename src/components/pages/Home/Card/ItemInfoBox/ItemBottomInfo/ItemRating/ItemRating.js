ad4fc2c82cecc06e629b152f22625451a28d6708
Markiian Sanotskye <marksanockiy@gmail.com>
2019-06-25 18:46:29 -0400

    DemoVersNotWorking

--- src/components/pages/Home/Card/ItemInfoBox/ItemBottomInfo/ItemRating/ItemRating.js
+++ src/components/pages/Home/Card/ItemInfoBox/ItemBottomInfo/ItemRating/ItemRating.js
@@ -2,16 +2,40 @@ import React from "react";
 import { ItemUlRating, ItemLiRating } from "./style";
 import RatingIcon from "../../../../../../shared/icons/RatingIcon/RatingIcon";
 
-const ItemRating = () => <ItemUlRating>{raitingList}</ItemUlRating>;
+export default class ItemRating extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { choosedRating: false };
+  }
 
-let raitingList = [];
+  toggleClass() {
+    this.setState({ choosedRating: !this.state.choosedRating });
+  }
 
-for (let i = 0; i < 5; i++) {
-  raitingList.push(
-    <ItemLiRating key={"ItemRaiting" + i}>
-      <RatingIcon />
-    </ItemLiRating>
-  );
+  render() {
+    let colorRating = "#000";
+
+    if (this.state.choosedRating) {
+      colorRating = "red";
+    }
+
+    const ItemRating = () => <ItemUlRating>{raitingList}</ItemUlRating>;
+
+    let raitingList = [];
+
+    for (let i = 0; i < 5; i++) {
+      raitingList.push(
+        <ItemLiRating
+          className="bla"
+          key={"ItemRaiting" + (i + 1)}
+          data-place={i}
+          //onClick={this.toggleClass.bind(this)}
+        >
+          <RatingIcon fill={colorRating} />
+        </ItemLiRating>
+      );
+    }
+
+    return <ItemRating />;
+  }
 }
-
-export default ItemRating;
